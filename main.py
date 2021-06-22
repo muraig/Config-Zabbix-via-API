1f41449bc458ad455e393511405dadae04869e8f
Aleksey Chernyshenko <53223732+che-a@users.noreply.github.com>
2021-06-22 14:41:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,29 +2,74 @@ import json
 import requests
 
 
-def zbx_auth(auth_file):
-    """Аутентификация на Zabbix-сервере"""
+def zbx_load_auth_from_file(auth_file):
+    """Загрузка учетных данных Zabbix-сервера из JSON-файла, используя которые будет получен токен."""
+    with open(auth_file, 'r', encoding='utf-8') as af:
+        auth_data = json.load(af)
+        auth_data["url"] = auth_data["protocol"].lower() + "://" + auth_data["ip"] + "/api_jsonrpc.php"
+        return auth_data
 
-    # загрузить из json
-    with open(auth_file, 'r', encoding='utf-8') as af:  # открываем файл на чтение
-        zbx = json.load(af)  # загружаем из файла данные в словарь data
 
-    url = f'http://{zbx["ip"]}/api_jsonrpc.php'
-    headers = {'content-type': 'application/json-rpc'}
+def zbx_apiinfo_version(url, id=1):
+    """Получении версии Zabbix и его API"""
+    request_json = f"""
+        {{
+            "jsonrpc":"2.0",
+            "method":"apiinfo.version",
+            "id":{id},
+            "auth":null,
+            "params":{{}}
+        }}
+    """
+    r = requests.post(url, headers = {'content-type': 'application/json-rpc'}, data=request_json)
+    return json.loads(r.content)
+
+
+def zbx_get_token(url, login, password, id=1):
+    """Аутентификация (получение токена) на Zabbix-сервере."""
+
     request_json = f"""
         {{
             "jsonrpc": "2.0",
             "method": "user.login",
             "params": {{
-                "user": "{zbx["login"]}",
-                "password": "{zbx["password"]}"
+                "user": "{login}",
+                "password": "{password}"
             }},
-            "id": 1,
+            "id": {id},
             "auth": null
         }}
     """
-    r = requests.post(url, headers=headers, data=request_json)
-    return json.loads(r.content)["result"]
+    r = requests.post(url, headers={'content-type': 'application/json-rpc'}, data=request_json)
+    return json.loads(r.content)
 
 
-print(zbx_auth('zabbix.json'))
+def zbx_hostgroup_create(url, token, group_name, id=1):
+    """Создание группы узлов сети"""
+    request_json = f"""
+        {{
+            "jsonrpc": "2.0",
+            "method": "hostgroup.create",
+            "params": {{
+                "name": "{group_name}"
+            }},
+            "auth": "{token}",
+            "id": {id}
+        }}
+    """
+    r = requests.post(url, headers={'content-type': 'application/json-rpc'}, data=request_json)
+    return json.loads(r.content)
+
+
+def main():
+    zbx = zbx_load_auth_from_file('zabbix.json')
+    zbx["version"] = zbx_apiinfo_version(zbx["url"])["result"]
+    zbx["token"] =  zbx_get_token(zbx["url"], zbx["login"], zbx["password"])["result"]
+    print(zbx)
+
+    for i in range(1, 11):
+        zbx_hostgroup_create(zbx["url"], zbx["token"], f"FIT Python group {i}")
+
+
+if __name__ == "__main__":
+    main()
